4de6739c5f04a896ade3be15539cfb05e4e0bb9b
govardhanakrishnapriya <govardhanakrishnapriya04@gmail.com>
2026-08-22 19:32:30 +0530

    Connect deployed frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -38,8 +38,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://cardiosecure.onrender.com"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
